a98c4fa976b38a59f1e5acdb4c40b1a8ce7386bc
pooja9872 <pooja_fw10_055@masai.school>
2021-12-07 20:21:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function App() {
+  const [details, setDetails] = useState([]);
+
+
+  const fetchDetails = async () => {
+    const { data } = await axios.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=845401&date=07-12-2021");
+    // const data = response.data;
+    // const res = data.data;
+    console.log("response", data.sessions);
+
+    // console.log(response.data[0]);
+
+    const details = data.sessions;
+
+
+    setDetails(details);
+
+
+  }
+
+  useEffect(() => {
+    fetchDetails();
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Covid Centers In Motihari</h1>
+      <div className="cities">
+        {details.map((el, index) => (
+          <div key={index} className="cities_content">
+            <p>{el.address}</p>
+            <p>Fee : {el.fee_type}</p>
+            <p>Age Limit : {el.min_age_limit}</p>
+            <p>Start Time from:  {el.from} to {el.to}</p>
+            <p>Vaccine Name: {el.vaccine}</p>
+          </div>
+        ))}
+
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
+
+
